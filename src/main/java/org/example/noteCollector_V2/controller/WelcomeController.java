d9a6f1b46ff3abf2bb299df007d74a05239c9a44
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-10-05 15:53:15 +0530

    created REST API

---
+++ src/main/java/org/example/noteCollector_V2/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package org.example.noteCollector_V2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/v1/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String viewWelcomeScreen(){
+        return "welcome";
+    }
+}

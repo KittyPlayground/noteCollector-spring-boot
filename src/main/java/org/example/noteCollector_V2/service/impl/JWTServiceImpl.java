f49be330affd80f8e99a3e4184103ed06da664f6
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-10-12 16:20:37 +0530

    Handle JWT : Generate Token

--- src/main/java/org/example/noteCollector_V2/service/impl/JWTServiceImpl.java
+++ src/main/java/org/example/noteCollector_V2/service/impl/JWTServiceImpl.java
@@ -2,6 +2,7 @@ package org.example.noteCollector_V2.service.impl;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.example.noteCollector_V2.service.JWTService;
@@ -10,26 +11,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JWTServiceImpl implements JWTService {
     @Value("${spring.jwtKey}")
     String jwtKey;
-
     @Override
     public String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
     @Override
-    public String generateToken(UserDetails user) {
-        return "";
+    public String generateToken(UserDetails userDetails) {
+        return genToken(new HashMap<>(),userDetails);
     }
 
     @Override
     public boolean validateToken(String token, UserDetails userDetails) {
-        return false;
+        String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     @Override
@@ -40,12 +44,29 @@ public class JWTServiceImpl implements JWTService {
         final Claims claims = getClaims(token);
         return claimsResolve.apply(claims);
     }
+
     private Claims getClaims(String token) {
-        return Jwts.parser().setSigningKey(getKey()).build().parseClaimsJwt(token)
+        return Jwts.parser().setSigningKey(getSignKey()).build().parseClaimsJwt(token)
                 .getBody();
     }
-    private Key getKey() {
+    private Key getSignKey() {
         byte [] decodedJWT = Decoders.BASE64.decode(jwtKey);
         return Keys.hmacShaKeyFor(decodedJWT);
+    }
+    private String genToken(Map<String, Object> genClaims, UserDetails userDetails) {
+        genClaims.put("role",userDetails.getAuthorities());
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + 1000 * 600);
+        return Jwts.builder().setClaims(genClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+    private boolean isTokenExpired(String token) {
+        return getExpirationDate(token).before(new Date());
+    }
+    private Date getExpirationDate(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
